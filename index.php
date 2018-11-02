1a2a5de0fcda929ff4e879ebd891a32af47e6b48
Meija Lohiniva <mlohiniva@hotmail.com>
2018-11-02 11:26:28 +0200

    Initial commit

--- index.php
+++ index.php
@@ -6,5 +6,9 @@
  * Time: 9.23
  */
 $number = 1;
+$array = [1, 3, 5, 9];
 $number = $number + 12;
+var_dump($array);
+echo 'test' . $number . '<br>';
+$number = $number + 13;
 echo 'test' . $number;
